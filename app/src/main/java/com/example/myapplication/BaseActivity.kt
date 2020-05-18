f7f1bf7f54e9b65eae2d9da6271e76487bcc398b
wanglinbo-git <441699677@qq.com>
2020-05-18 15:57:13 +0800

    shop 2.0 开发完毕

--- app/src/main/java/com/example/myapplication/BaseActivity.kt
+++ app/src/main/java/com/example/myapplication/BaseActivity.kt
@@ -3,7 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-
+//shop2.0开发
  open class BaseActivity(var layoutid:Int) : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
